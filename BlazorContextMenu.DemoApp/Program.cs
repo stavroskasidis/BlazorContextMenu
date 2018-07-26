237b9e1fa3dd5870db3ca25c221817ef7f8db5a8
stavroskasidis <stavroskasidis@outlook.com>
2018-07-27 00:11:54 +0300

    updated demo app

--- BlazorContextMenu.DemoApp/Program.cs
+++ BlazorContextMenu.DemoApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Blazor.Browser.Rendering;
 using Microsoft.AspNetCore.Blazor.Browser.Services;
+using Microsoft.AspNetCore.Blazor.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 
@@ -7,14 +8,13 @@ namespace BlazorContextMenu.DemoApp
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            var serviceProvider = new BrowserServiceProvider(services =>
-            {
-                // Add any custom services here
-            });
-
-            new BrowserRenderer(serviceProvider).AddComponent<App>("app");
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
     }
 }
