60a460ae5be6a11184ff3c3d1816045bcba363a0
stavroskasidis <stavroskasidis@outlook.com>
2018-07-28 23:27:34 +0300

    updated demo app

--- BlazorContextMenu.DemoApp/Startup.cs
+++ BlazorContextMenu.DemoApp/Startup.cs
@@ -11,6 +11,7 @@ namespace BlazorContextMenu.DemoApp
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddBlazorContextMenu();
         }
 
         public void Configure(IBlazorApplicationBuilder app)
