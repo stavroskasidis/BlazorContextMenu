238cee1a0f860faa1d048b32fdc55c8fdd71524f
Stavros Kasidis <stavroskasidis@gmail.com>
2019-03-06 17:18:15 +0200

    aded prism.js

--- DemoApp/BlazorContextMenu.DemoApp/Startup.cs
+++ DemoApp/BlazorContextMenu.DemoApp/Startup.cs
@@ -11,7 +11,18 @@ namespace BlazorContextMenu.DemoApp
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddBlazorContextMenu();
+            services.AddBlazorContextMenu(options=>
+            {
+                options.ConfigureTemplate("dark", template =>
+                {
+                    template.MenuCssClass = "dark-menu";
+                    template.MenuItemCssClass = "dark-menu-item";
+                    template.MenuItemDisabledCssClass = "dark-menu-item--disabled";
+                    template.SeperatorHrCssClass = "dark-menu-seperator-hr";
+                    template.MenuItemWithSubMenuCssClass = "dark-menu-item--with-submenu";
+                    template.Animation = Animation.FadeIn;
+                });
+            });
         }
 
         public void Configure(IComponentsApplicationBuilder app)
