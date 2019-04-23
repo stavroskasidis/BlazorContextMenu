a0a3ad885fdfb64fd4f7a2a7c564e9f1b0c02911
Stavros Kasidis <stavroskasidis@gmail.com>
2019-04-23 12:19:19 +0300

    added dynamic items showcase

--- DemoApp/BlazorContextMenu.DemoApp/wwwroot/interop.js
+++ DemoApp/BlazorContextMenu.DemoApp/wwwroot/interop.js
@@ -15,17 +15,28 @@ if (window.Element && !Element.prototype.closest) {
 
 
 
-DemoApp_GetItemIndex = function (targetItemId) {
+DemoApp_GetRowIndex = function (targetItemId) {
     var targetItem = document.getElementById(targetItemId);
     var tr = targetItem.closest("tr");
     if (tr.parentNode.tagName == "THEAD") {
         return -1;
     }
-
     var children = tr.parentNode.childNodes;
     var num = 0;
     for (var i = 0; i < children.length; i++) {
         if (children[i] == tr) return num;
+        if (children[i].nodeType == 1) num++;
+    }
+    return -1;
+};
+
+DemoApp_GetListItemIndex = function (targetItemId) {
+    var targetItem = document.getElementById(targetItemId);
+    var li = targetItem.closest("li");
+    var children = li.parentNode.childNodes;
+    var num = 0;
+    for (var i = 0; i < children.length; i++) {
+        if (children[i] == li) return num;
         if (children[i].nodeType == 1) num++;
     }
     return -1;
