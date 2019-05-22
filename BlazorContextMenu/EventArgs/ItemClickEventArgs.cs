ecd014a9a45bcf0839d470a8e42eaa425fc1340e
Stavros Kasidis <stavroskasidis@gmail.com>
2019-05-22 19:07:24 +0300

    in progress

--- BlazorContextMenu/EventArgs/ItemClickEventArgs.cs
+++ BlazorContextMenu/EventArgs/ItemClickEventArgs.cs
@@ -17,10 +17,29 @@ namespace BlazorContextMenu
             MenuItem = menuItem;
         }
 
+        /// <summary>
+        /// The args of the mouse event.
+        /// </summary>
         public UIMouseEventArgs MouseEvent { get; protected set; }
+        
+        /// <summary>
+        /// If set to true, then the ContextMenu will not close after clicking on the item.
+        /// </summary>
         public bool IsCanceled { get; set; }
+        
+        /// <summary>
+        /// The id of the <see cref="ContextMenu"/> that contains the triggering item.
+        /// </summary>
         public string ContextMenuId { get; protected set; }
+
+        /// <summary>
+        /// The id of the target element that the <see cref="ContextMenu"/> was triggered from.
+        /// </summary>
         public string ContextMenuTargetId { get; protected set; }
+
+        /// <summary>
+        /// The ElementRef of the item's li. To be passed to javascript interop.
+        /// </summary>
         public ElementRef MenuItemElement { get; protected set; }
         public Item MenuItem { get; protected set; }
     }
