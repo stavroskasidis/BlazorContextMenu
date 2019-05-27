6f4228111c70ef01f9d5a0554f2e8ad744d75e1d
Stavros Kasidis <stavroskasidis@gmail.com>
2019-05-27 12:18:50 +0300

    added event onapearing on contextmenu

---
+++ BlazorContextMenu/EventArgs/MenuAppearingEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorContextMenu
+{
+    public class MenuAppearingEventArgs
+    {
+        public MenuAppearingEventArgs(string contextMenuId, string contextMenuTargetId)
+        {
+            ContextMenuId = contextMenuId;
+            ContextMenuTargetId = contextMenuTargetId;
+        }
+
+        public string ContextMenuId { get; protected set; }
+
+        public string ContextMenuTargetId { get; protected set; }
+
+        /// <summary>
+        /// If set to true, then the menu will not appear.
+        /// </summary>
+        public bool IsCanceled { get; set; }
+    }
+}
