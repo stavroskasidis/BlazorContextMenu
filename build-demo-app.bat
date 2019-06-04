4c49f4342ae7ab81678240cee1ae864ec9a036b3
stavroskasidis <stavroskasidis@users.noreply.github.com>
2019-06-05 00:25:47 +0300

    Update build-demo-app.bat

--- build-demo-app.bat
+++ build-demo-app.bat
@@ -1,6 +1,7 @@
 @echo off
 cd "%~dp0"
 
-
-dotnet publish DemoApp\BlazorContextMenu.DemoApp.Server -c Release -o artifacts\demoapp
+dotnet build DemoApp\BlazorContextMenu.DemoApp.Server -c Release
+if %errorlevel% neq 0 exit /b %errorlevel%
+dotnet publish DemoApp\BlazorContextMenu.DemoApp.Server -c Release -o artifacts\demoapp --no-build
 if %errorlevel% neq 0 exit /b %errorlevel%
