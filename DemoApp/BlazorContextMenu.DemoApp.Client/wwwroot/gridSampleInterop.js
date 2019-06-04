821e46c415f0330b45eb1fad8cad5bf9a2c9cd43
stavroskasidis <stavroskasidis@outlook.com>
2019-06-05 00:01:24 +0300

    cleanup: added dynamic source loading

---
+++ DemoApp/BlazorContextMenu.DemoApp.Client/wwwroot/gridSampleInterop.js
@@ -0,0 +1,18 @@
+﻿var gridSampleInterop = (function () {
+    return {
+        getRowIndex: function (targetItemId) {
+            var targetItem = document.getElementById(targetItemId);
+            var tr = targetItem.closest("tr");
+            if (tr.parentNode.tagName == "THEAD") {
+                return -1;
+            }
+            var children = tr.parentNode.childNodes;
+            var num = 0;
+            for (var i = 0; i < children.length; i++) {
+                if (children[i] == tr) return num;
+                if (children[i].nodeType == 1) num++;
+            }
+            return -1;
+        }
+    };
+})();
